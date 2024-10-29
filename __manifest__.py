6246c7aab9eecb19def9a8922dcc3c9f1f3e1a9c
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-10-29 15:28:42 -0300

    Updated manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "aaaaa",
+    'name': "Hide cash on POS closure",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Just hides cash values on POS closure""",
 
     'description': """
-        Long description of module's purpose
+        Just hides cash values on POS closure
     """,
 
     'author': "My Company",
@@ -16,14 +15,13 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Point of Sale',
+    'version': '1.0',
     'depends': ['point_of_sale'],
-
-    # always loaded
-    'data': [
-    ],
+    'assets': {
+        "point_of_sale._assets_pos": [
+            "/hide_cash_on_pos_closure/static/src/xml/closing_popup.xml",
+        ],
+    },
 
 }
